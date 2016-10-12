169291acc1a809e7fe6927cd00cc56f8e1fe1f2d
Mahesh Gahlot <Gahlot@users.noreply.github.com>
2016-10-12 12:35:44 +0530

    Revert "Revert "Add arguments functionality and optimization of code""

--- MaxConversation.py
+++ MaxConversation.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
-res = {}
-with open('logs') as fob:
-    for line in fob:
-        if line.find('<') != -1:
-            k = line[12:].split('>')[0]
-            res[k] = res.get(k,{})
-with open('logs') as fob:
-    for line in fob:
-        s = line.find('<')
-        if s != -1:
-            k = line[12:].split('>')
-            for call in k[1].split(' '):
-                so = call.strip(',:')
-                if so in res:
-                    res[k[0]][so]=res[k[0]].get(so,0) + 1
+import sys
 
+res = {}
+files=[]
+#files list passed as arguments in shell
+for i in sys.argv[1:]:
+	files.append(i)
+
+if len(files)==0:
+	print('Also input filenames as arguments')
+	exit()
+
+for i in files:
+	#open file and process users
+	with open(i) as fob:
+		for line in fob:
+		    if line.find('<') != -1:
+		        k = line[12:].split('>')[0]
+		        res[k] = res.get(k,{})
+		    s = line.find('<')
+		    if s != -1:
+		        k = line[12:].split('>')
+		        for call in k[1].split(' '):
+		            so = call.strip(',:')
+		            if so in res:
+		                res[k[0]][so]=res[k[0]].get(so,0) + 1
+
+#result
 for i in res:
     if res[i] == {}:
         print(i,"not spoke")
