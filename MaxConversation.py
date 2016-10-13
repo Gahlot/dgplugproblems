1f47589d6139bbb90cbd6a11eb07f61c9aa118ac
vibhcool <vibhcool@gmail.com>
2016-10-13 12:37:51 -0400

    applied argparse library

--- MaxConversation.py
+++ MaxConversation.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
-import sys
+import argparse
 
-res = {}
+res={}
+
+parser=argparse.ArgumentParser(description='add file to find who spoke and who didn\'t')
+k=open('logs','r')
+parser.add_argument('-g',metavar='FILES',type=argparse.FileType('r'),nargs='*',default=[k])
+
+parser.add_argument('-f',metavar='FILE',type=argparse.FileType('r'),nargs='?',default=k)
+
+args = parser.parse_args()
+
 files=[]
-#files list passed as arguments in shell
-for i in sys.argv[1:]:
-	files.append(i)
+
+if args.f:
+	files=[args.f]
+elif args.f is None:
+	files=[k]
+elif args.g:
+	files=args.g
 
 if len(files)==0:
 	print('Also input filenames as arguments')
@@ -13,18 +26,18 @@ if len(files)==0:
 
 for i in files:
 	#open file and process users
-	with open(i) as fob:
-		for line in fob:
-		    if line.find('<') != -1:
-		        k = line[12:].split('>')[0]
-		        res[k] = res.get(k,{})
-		    s = line.find('<')
-		    if s != -1:
-		        k = line[12:].split('>')
-		        for call in k[1].split(' '):
-		            so = call.strip(',:')
-		            if so in res:
-		                res[k[0]][so]=res[k[0]].get(so,0) + 1
+	for line in i:
+	    if line.find('<') != -1:
+	        k = line[12:].split('>')[0]
+	        res[k] = res.get(k,{})
+	    s = line.find('<')
+	    if s != -1:
+	        k = line[12:].split('>')
+	        for call in k[1].split(' '):
+	            so = call.strip(',:')
+	            if so in res:
+	                res[k[0]][so]=res[k[0]].get(so,0) + 1
+	i.close()
 
 #result
 for i in res:
